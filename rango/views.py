5c824bb02c06f13025298e49b46ec2572db2a8db
Youssef Moawad <2170232m@student.gla.ac.uk>
2017-02-08 21:34:02 +0000

    after chapter 10

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -12,10 +13,16 @@ def index(request):
 
     context_dict = {'categories': category_list, 'pages': pages_list}
 
+    # Handle the cookies
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html', {})
+    visitor_cookie_handler(request)
+
+    return render(request, 'rango/about.html', {'visits': request.session['visits']})
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -195,3 +202,26 @@ def user_logout(request):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html', {})
+
+# Helper function
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Helper function
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if(datetime.now() - last_visit_time).seconds > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
